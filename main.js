18d1fa869939feaa76836ce66f1580b75d322e81
bornittah <bornittah@gmail.com>
2022-07-11 11:18:38 +0300

    implement collapsible div

--- main.js
+++ main.js
@@ -1,11 +1,13 @@
-const accordion = document.getElementsByClassName('content-container');
-const answer = document.querySelectorAll('.answer');
+let question = document.getElementsByClassName('question');
 
-for(let i = 0; i < accordion.length; i++){
-    accordion[i].addEventListener('click', () => {
-        this.classList.toggle('active');
-        answer.style.height = "150px";
-        
-        
-    });
-};
+for (let i = 0; i < question.length; i++) {
+  question[i].addEventListener('click', function () {
+    this.classList.toggle('active');
+    let answer = this.nextElementSibling;
+    if (answer.style.display === 'block') {
+      answer.style.display = 'none';
+    } else {
+      answer.style.display = 'block';
+    }
+  });
+}
